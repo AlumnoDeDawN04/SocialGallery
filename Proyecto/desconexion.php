8d6480f3dd776487d44a2c844a2a957ddfe61284
juanma <100006305.joan23@fje.edu>
2019-10-25 17:33:15 +0200

    Quitar proyecto

--- Proyecto/desconexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	//sesion destroy elimina los datos de la sesion para salir de la sesion
-	session_destroy();
-	header("location:home.php")
-?>
--- Proyecto/inserts_usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php
-include"conexion.php";
-$nombre=$_POST["user"];
-$pwd=$_POST["pwd"];
-$enc=md5($pwd);
-		$query="insert into usuarios (user,password) values ('$nombre','pwd')";
-		$consulta=mysqli_query($conn,$query);
-		header("location:home.php");
-	?>
